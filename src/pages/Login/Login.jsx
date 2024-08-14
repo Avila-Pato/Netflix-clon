3cac92a4fa588fdbd9820a5a88a5fabad958072b
Avila-Pato <p.avilaf1998@gmail.com>
2024-08-14 16:36:10 -0400

    Agreanado spinner animacion para carga de login y se vea mas interactiva

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import './Login.css'
 import logo from '../../assets/assets/logo.png'
 import { useState } from 'react'
 import {login , signup } from '../../firebase'
+import netflix_spinner from '../../assets/assets/netflix_spinner.gif'
 
 
 const Login = () => {
@@ -12,17 +13,25 @@ const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  // agregando el sppiner
+  const [loading, setLoading] = useState(false)
+
   const user_auth = async(event) => {
     event.preventDefault()
+    setLoading(true)
     if(singState === 'Inicia sesión'){
       await login(email, password)
     }else{
       await signup(name, email, password)
     }
+    setLoading(false) 
   }
 
 
   return (
+    loading?<div className='login-spinner'>
+        <img src={netflix_spinner} alt="" />
+    </div>:
     <div className='login'>
       <img src={logo} alt=""className='login-logo'/>
       <div className="login-form">
